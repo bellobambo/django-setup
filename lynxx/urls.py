22233dc2662dfa46edb6212dd7cd384c0af4077b
bellobambo <bellobambo21@gmail.com>
2023-07-17 16:19:19 +0100

    django project setup

--- lynxx/urls.py
+++ lynxx/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
 path('' , views.home , name="" ),
 
+path('article/<int:pk>/' , views.singular_article , name='article')
+
 ]
--- lynxx/views.py
+++ lynxx/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Article
+
 def home(request):
 
-    return render(request, 'lynxx/index.html')
+    articles = Article.objects.all()
+
+    context = {'articles' : articles}
+
+    return render(request, 'lynxx/index.html' , context=context)
+
+
+
+def singular_article(request , pk):
+
+    article = Article.objects.get(id=pk)
+
+    context = {'article' : article}
+
+    return render(request, 'lynxx/article.html' , context=context)
+
